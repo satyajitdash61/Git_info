270078c91180a7afa1012da0dae42776e547e007
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-20 00:24:11 +0530

    loading

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-// $(document).ready(function(){
-//     alert("hi");
-// })
+let gitID;
+$("#btn").click(function(){
+    gitID=$("#input").val();
+    alert(gitID);
+})
+
+const client_id = "Iv1.50e6543175e493fc";
+const client_secret = "5a84fa87938506347b27b5c8112f680200a0efc4";
