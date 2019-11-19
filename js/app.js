9510b4139c05a9c319ee83cd195f3417a2f0ea79
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-11-20 00:31:09 +0530

    updating

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 let gitID;
-$("#btn").click(function(){
-    gitID=$("#input").val();
-    alert(gitID);
-})
+$("#btn").click(function() {
+  gitID = $("#input").val();
+  alert(gitID);
+});
 
 const client_id = "Iv1.50e6543175e493fc";
 const client_secret = "5a84fa87938506347b27b5c8112f680200a0efc4";
+
+const fetchUsers = async user => {
+  const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&
+    client_secret=${client_secret}`);
+};
